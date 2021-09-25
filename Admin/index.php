396c1dcf30f915583bf10b18e838936a666ddb2e
unknown <ajeetkumar15.yadav@gmail.com>
2021-09-25 17:37:32 +0530

    first commit

---
+++ Admin/index.php
@@ -0,0 +1,96 @@
+<?php
+include('includes/header.php');
+include('includes/topbar.php');
+include('includes/sidebar.php');
+?>
+
+
+
+ <!-- Content Wrapper. Contains page content -->
+ <div class="content-wrapper ">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Dashboard</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <!-- <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard v1</li>
+            </ol> -->
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <div class="container-fluid">
+    <div class="row">
+          <div class="col-xlg-3 col-lg-3 col-md-3 col-sm-6 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>5</h3>
+
+                <p>Course</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <a href="#" class="small-box-footer"></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-xlg-3 col-lg-3 col-md-3 col-sm-6 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>14</h3>
+
+                <p>Students</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+              <a href="#" class="small-box-footer"></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-xlg-3 col-lg-3 col-md-3 col-sm-6 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>2</h3>
+
+                <p>User Registrations</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="#" class="small-box-footer"></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-xlg-3 col-lg-3 col-md-3 col-sm-6 col-6">
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>6</h3>
+
+                <p>Unique Visitors</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-pie-graph"></i>
+              </div>
+              <a href="#" class="small-box-footer"></a>
+            </div>
+            
+          </div>
+          <!-- ./col -->
+        </div>
+</div>
+
+<?php
+include('includes/footer.php');
+?>
